bc8542450999e33bf5f137bcaad72a3e02016920
Abhinav Aryan <77077109+07abhi9av@users.noreply.github.com>
2024-03-17 16:16:16 +0530

    Create KNAPSACK.py

---
+++ KNAPSACK.py
@@ -0,0 +1,19 @@
+0-1 KNAPSACK ALGORITHM
+You are given N items that cannot be broken. Each item has a weight and value associated with it.
+You also have a KnapSack of capacity W.
+Find the maximum value of items you can collect in the KnapSack so that the total weight does not exceed W.
+Input
+First line contains the values N and W.
+Second line contains N integers denoting the weights.
+Last line contains N integers denoting the values.
+Output
+Print the maximum value that can be collected with total weight less than or equal to W.
+
+Example
+Input:
+3 5
+1 2 3
+1 5 3
+
+Output:
+8
